d4407667720cefafdbfc9223db4f1ef68994975b
awfink <awfink@comtechsolutions.net>
2014-06-05 01:00:00 -0600

    Created LeapYear test and function ...

--- Euler/EulerClass.cs
+++ Euler/EulerClass.cs
@@ -14,14 +14,48 @@ namespace EulerNamespace
             return(x.ToString());
         }
 
-        public int CountSundays()
+        //Solve Euler problem #99
+        //Determine the number of Sundays between 1/1/1901 and 12/31/2000
+        //Without using the DayOfWeek command.
+        public int CountSundaysFDOM()
         {
-            return (CountSundays_TheEasyWay());
+            var dy=1;
+            var mn=1;
+            var CurrentDayOfWeek = 2;                   //Start at Tuesday (2) with a Sunday  being 0.
+
+            for (var yr = 1900; yr <= 2000; yr++)
+            {
+                var DaysInYear = 365;
+                var isLeapYear = false;
+
+                isLeapYear = IsLeapYear(yr);
+                if (isLeapYear) DaysInYear = 366;
+
+            }
+            return (CountSundaysFDOM_TheEasyWay());
+        }
+
+        public bool IsLeapYear(int yr)
+        {
+            if (yr % 4 == 0)
+            {
+                // Test if century
+                if( yr % 100 == 0 ) {
+                    if( yr % 400 == 0 ) {
+                        return(true);
+                    }
+                }
+                else {
+                    return(true);
+                }
+            }
+            return (false);
         }
 
         // Temporary procedure to loop through all days between 1/1/1901 and 2/31/2000
-        // just to find the number of sundays for testing purposes.
-        public int CountSundays_TheEasyWay()
+        // just to find the number of Sundays for testing purposes.
+        // Function modified to exclude Sundays that are not the first day of the month.
+        public int CountSundaysFDOM_TheEasyWay()
         {
             var StartDate = DateTime.Parse("1/1/1901");
             var EndDate = DateTime.Parse("12/31/2000");
@@ -29,7 +63,13 @@ namespace EulerNamespace
 
             for (var dt = StartDate; dt <= EndDate; dt = dt.AddDays(1))
             {
-                if (dt.DayOfWeek.ToString() == "Sunday") { Days += 1; };
+                if (dt.DayOfWeek.ToString() == "Sunday") 
+                {
+                    if (dt.Day == 1)
+                    {
+                        Days += 1;
+                    }
+                };
             }
 
             return (Days);
--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -16,11 +16,38 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void Test_Sundays_Between_01_Jan_1901_AND_12_31_2000()
+        public void Test_LeapYear1900()
         {
             var csObj = new CountingSundays();
-            var cnt = csObj.CountSundays();
-            Assert.AreEqual(5218, cnt);
+            Assert.AreEqual(false, csObj.IsLeapYear(1900));
         }
+
+        [TestMethod]
+        public void Test_LeapYear1904()
+        {
+            var csObj = new CountingSundays();
+            Assert.AreEqual(true, csObj.IsLeapYear(1904));
+        }
+
+        [TestMethod]
+        public void Test_AllLeapYearsBetween1900And2000()
+        {
+            var csObj = new CountingSundays();
+            var LeapYearCount = 0;
+            for (var i = 1900; i <= 2000; i++)
+            {
+                if (csObj.IsLeapYear(i)) LeapYearCount += 1;
+            }
+            Assert.AreEqual(25, LeapYearCount);
+        }
+
+        [TestMethod]
+        public void Test_Sundays_On_The_First_Day_Of_The_Month_Between_01_Jan_1901_AND_12_31_2000()
+        {
+            var csObj = new CountingSundays();
+            var cnt = csObj.CountSundaysFDOM();
+            Assert.AreEqual(171, cnt);
+        }
+
     }
 }
