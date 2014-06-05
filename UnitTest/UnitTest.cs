f93937fe392680adb866bd116d289d0c82395a2b
awfink <awfink@comtechsolutions.net>
2014-06-05 01:00:00 -0600

    Created a new test for # of Sundays.  Proc doesn't exist yet and I don't know how many Sundays there are (yet).

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -14,5 +14,13 @@ namespace UnitTest
             var dow = csObj.GetDayOfWeek(DateTime.Parse("1/1/1900"));
             Assert.AreEqual("Monday", dow);
         }
+
+        [TestMethod]
+        public void Test_Sundays_Between_01_Jan_1901_AND_12_31_2000()
+        {
+            var csObj = new CountingSundays();
+            var cnt = csObj.CountSundays();
+            Assert.AreEqual(65, cnt);
+        }
     }
 }
